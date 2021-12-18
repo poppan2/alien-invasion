9210722ee5b0469f97a34307a1d510eff2b20ba3
poppan2 <ye_aung94@yahoo.com>
2021-12-17 20:51:29 -0800

    update the background color

--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ class Settings():
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (210, 210, 210)
+        self.bg_color = (100, 100, 100)
