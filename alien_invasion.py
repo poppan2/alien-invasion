19b1904165e33214e00feb6b8fff0dbf130c7996
poppan2 <ye_aung94@yahoo.com>
2021-12-17 20:52:04 -0800

    import ship class and update

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,23 +1,34 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
+
     # Initialize pygame, settings and a screen object
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # Make a ship
+    ship = Ship(screen)
+
     # Start the main Loop for the game
     while True:
+
         # Watch for keyboard and mouse events
         for event in pygame.event.get():
+
             # Exit the game when the user clicks the game window's close button
             if event.type == pygame.QUIT:
                 sys.exit()
-            # Change the backround color of the screen
+
+            # Change the backround color of the screen 
             screen.fill(ai_settings.bg_color)
+            # Draw the ship on screen
+            ship.blitme()
+
             # Make the most recently drawn screen visible
             pygame.display.flip()
 
