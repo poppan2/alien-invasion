70fa8a649bf9b8ebfb388ac199fb31f10b776112
poppan2 <ye_aung94@yahoo.com>
2021-12-19 12:02:23 -0800

    adjust ship movement using ship speed setting & add screen edges conditions

--- ship.py
+++ ship.py
@@ -1,10 +1,10 @@
 import pygame
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position."""
         self.screen = screen
-
+        self.ai_settings = ai_settings
         # Load the ship image and get its rect.
         # pygame let us treat game elements like rectangles (rects)
         self.image = pygame.image.load('./images/Spaceship_1.bmp')
@@ -17,17 +17,23 @@ class Ship():
         # the x-coordinate of the ship's center
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-
+        self.center = float(self.rect.centerx)
         # Movement flag
         self.moving_right = False
         self.moving_left = False
     
     def update(self):
         """Update the ship's position based on the movement flag."""
-        if self.moving_right:
-            self.rect.centerx += 1
-        if self.moving_left:
-            self.rect.centerx -= 1
+        # Update the ship's center value
+        # Check the position of the ship before changing the value
+        # Stop moving right if the x-coordinate value has reached the right edge of the screen
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed
+        # Stop moving left if the x-coordinate value has reached 0
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed
+
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship at its current location."""
