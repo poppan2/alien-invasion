00a179aa385dec902b38a38dca1cffe1dfa226af
poppan2 <ye_aung94@yahoo.com>
2021-12-19 14:32:05 -0800

    add bullet settings

--- settings.py
+++ settings.py
@@ -9,3 +9,9 @@ class Settings():
     
         # ship settings
         self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 10
+        self.bullet_color = (255, 0, 0)
