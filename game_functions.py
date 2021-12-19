00ed5b9b340cc5b6e8ecd82825386851c9df7ec9
poppan2 <ye_aung94@yahoo.com>
2021-12-19 12:44:36 -0800

    refactor the key press and key release conditions as separate functions

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,22 @@
 import sys
 import pygame
 
+def check_keydown_event(event, ship):
+    """Respond to key press"""
+    if event.key == pygame.K_RIGHT:
+        # Move the ship to the right.
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        # Move the ship to the left
+        ship.moving_left = True
+
+def check_keyup_event(event, ship):
+    """Respond to key release"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """Respond to keypresses and mouse events"""
     for event in pygame.event.get():
@@ -9,19 +25,10 @@ def check_events(ship):
             sys.exit()
         # If the arrow Keys are pressed, we move the ship
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # Move the ship to the right.
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                # Move the ship to the left
-                ship.moving_left = True
+            check_keydown_event(event, ship)
         # When the arrow keys are released, we stop the movement
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
-
+            check_keyup_event(event,ship)
 
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to the new screen"""
@@ -29,7 +36,7 @@ def update_screen(ai_settings, screen, ship):
     screen.fill(ai_settings.bg_color)
 
     # Draw the ship on screen
-    ship.blitme()
+    ship.blitship()
 
     # Make the most recently drawn screen visible
     pygame.display.flip()
