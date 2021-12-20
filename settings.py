530ef7ffeb8d7b3851c4f57e0f4d223bcc2a6d69
poppan2 <ye_aung94@yahoo.com>
2021-12-20 15:17:28 -0800

    add alien speed to settings

--- settings.py
+++ settings.py
@@ -15,4 +15,7 @@ class Settings():
         self.bullet_width = 3
         self.bullet_height = 10
         self.bullet_color = (255, 0, 0)
-        self.bullets_limit = 4
+        self.bullets_limit = 10
+
+        # Alien settings
+        self.alien_speed = 1
