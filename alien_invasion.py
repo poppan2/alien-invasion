1b756d951d72f47f674c13ea7a6253dddd485ed1
poppan2 <ye_aung94@yahoo.com>
2021-12-21 12:13:55 -0800

    add update_alien method to main game loop

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,6 +26,7 @@ def run_game():
         gf.check_events(ai_settings, screen, ship, bullets)
         gf.update_ship(ship)
         gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
