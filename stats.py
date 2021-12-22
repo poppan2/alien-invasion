917c711c4cbf807724c93ab471f3b5814f263550
poppan2 <ye_aung94@yahoo.com>
2021-12-22 14:30:27 -0800

    add score, lvl, & high_score

--- stats.py
+++ stats.py
@@ -3,7 +3,9 @@ class Game_Stats():
         self.ai_settings = ai_settings
         self.reset_stats()
         self.run_game = False
+        self.high_score = 0
 
     def reset_stats(self):
         self.ships_left = self.ai_settings.ship_lives
         self.score = 0
+        self.lvl = 1
