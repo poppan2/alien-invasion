094b83aea3a494afb6bd65cbd80889504aec7e19
poppan2 <ye_aung94@yahoo.com>
2021-12-22 10:41:12 -0800

    set the run_game to False

--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@ class Game_Stats():
     def __init__(self, ai_settings):
         self.ai_settings = ai_settings
         self.reset_stats()
-        self.run_game = True
+        self.run_game = False
 
     def reset_stats(self):
         self.ships_left = self.ai_settings.ship_lives
