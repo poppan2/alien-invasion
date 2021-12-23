ad44348d4ee956eac7c069ad21ca85d2b761466e
poppan2 <ye_aung94@yahoo.com>
2021-12-22 16:21:31 -0800

    import Sprite to create a group of ship objects

--- ship.py
+++ ship.py
@@ -1,10 +1,12 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
     """A class to create a Space Ship"""
 
     def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position."""
+        super(Ship, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
         # Load the ship image and get its rect.
